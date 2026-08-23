ed96c210e180c71280b769e3146044e05329bfec
Thomas Bassetto <tbassetto@gmail.com>
2026-08-23 11:46:43 +0200

    Improve SEO accessibility and image assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://www.frenchtech.no',
   adapter: vercel(),
+  integrations: [sitemap()],
 });
-
---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+
+const getRobotsTxt = (sitemapUrl: URL) => `User-agent: *
+Allow: /
+
+Sitemap: ${sitemapUrl.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapUrl = new URL('sitemap-index.xml', site);
+
+  return new Response(getRobotsTxt(sitemapUrl), {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+};
